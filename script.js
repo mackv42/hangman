15c06cb12fe9aeaf88b08e1b262e40f1cd9a9cfe
Vincent Mackay <mackv42@gmail.com>
2019-06-28 12:16:19 -0700

    ai returns most frequent letters

--- script.js
+++ script.js
@@ -1,86 +1,105 @@
 
-	document.getElementById("computerPlay").onclick = function(e){
-		e.preventDefault();
-		computerPlay()
+document.getElementById("computerPlay").onclick = function(e){
+	e.preventDefault();
+	computerPlay( document.getElementById("phrase").value );
+};
+
+
+
+String.prototype.replaceAt=function(index, l, character){
+   	return this.substr(0, index) + character + this.substr(index+l);
+};
+
+String.prototype.replaceAllWith = function( r, s ){
+	var str = this;
+		
+	while(str.search(keyword)!=-1){
+			str = str.replaceAt(str.indexOf( r ), keyword.length, s);
+	}
+
+	return str;
+}
+
+
+function questionMark( word ){
+	return word.replaceAllWith("_", "?");
+}
+
+
+// returns most common letters from array of strings
+function findMostCommonLetters( arr ){
+	let frequencyList = [];
+	for(let i=0; i<26; i++){
+		frequencyList.push(0);
+	}
+
+	for(let i=0; i<100; i++){
+		let word = arr[i];
+		for(let j=0; j<word.length; j++){
+				try{
+					frequencyList[ word.charCodeAt(j)-97 ] += 1;
+					
+				} catch ( e ){
+					console.log("failed");
+					continue;
+				}
+		}
 	};
 
+	console.log(frequencyList);
 
+	return frequencyList;
+}
 
-	String.prototype.replaceAt=function(index, l, character){
-	   	return this.substr(0, index) + character + this.substr(index+l);
-	};
+function convertToDictionary(arr){
+	let dict = {};
+	for(let i=0; i<arr.length; i++){
+		dict[String.fromCharCode(i+97)] = arr[i];
+	}
+	return dict;
+}
 
-	String.prototype.replaceAllWith = function( r, s ){
-		var str = this;
-			
-		while(str.search(keyword)!=-1){
-				str = str.replaceAt(str.indexOf( r ), keyword.length, s);
-		}
+var myData = [];
+function lookup( word ){
+	//word = word.replaceAllWith("_", "?");
+	var request = new XMLHttpRequest();
+	request.open('GET', "https://api.datamuse.com/words?sp=" + word, true);
+	var data = [];
+	var mostCommon = [];
+	request.onload = function () {
+		data = JSON.parse(this.response);
 
-		return str;
+		console.log(this.response);
+		
+		myData = findMostCommonLetters(data.map( d => { return d.word } ));
+		let keys = convertToDictionary(myData);
+		let keysSorted = Object.Keys(keys).sort(function(a,b){return keys[a]-keys[b]});
+		console.log(keysSorted);
+		myData = keysSorted;
 	}
 
+	request.send();
+}
 
-	function questionMark( word ){
-
+function computerPlay( word ){
+	//API calls
+	let foundWord = "";
+	for(let i=0; i<word.length; i++){
+		foundWord += "?";
 	}
-
-
-	// returns most common letters from array of strings
-	function findMostCommonLetters( arr ){
-		let frequencyList = [];
-		for(let i=0; i<26; i++){
-			frequencyList.push(0);
-		}
-
-		for(let i=0; i<100; i++){
-			let word = arr[i];
-			for(let j=0; j<word.length; j++){
-					try{
-						frequencyList[ word.charCodeAt(j)-97 ] += 1;
-						
-					} catch ( e ){
-						console.log("failed");
-						continue;
-					}
-			}
-		};
-
-		console.log(frequencyList);
-
-		return frequencyList;
+	let guessString = foundWord;
+	function guess(){
+		
 	}
-
-
-	var myData = [];
-	function lookup( word ){
-		//word = word.replaceAllWith("_", "?");
-		var request = new XMLHttpRequest();
-		request.open('GET', "https://api.datamuse.com/words?sp=" + word, true);
-		var data = [];
-		var mostCommon = [];
-		request.onload = function () {
-			data = JSON.parse(this.response);
-
-			console.log(this.response);
-			
-			myData = findMostCommonLetters(data.map( d => { return d.word } ));		
-		}
-
-		request.send();
-	}
-
-	function computerPlay(  ){
-
-		let word = document.getElementById("phrase").value;
-		//API calls
-		let foundWord = "";
-		for(let i=0; i<word.length; i++){
-			foundWord += "?";
-		}
+	function loop(){ // loops until find word or loses
 		lookup(foundWord);
-	}
+		if(guess(data[0])){
 
-	function humanPlay( word ){
-
+		}
+		console.log(convertToDictionary(myData));
 	}
+}
+
+function humanPlay( word ){
+
+}
